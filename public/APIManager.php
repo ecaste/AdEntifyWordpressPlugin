04c5725fd86cc16b5a61e2f2c269e274df1a4764
Pierrick Martos <pierrick@martos.me>
2014-10-13 14:19:41 +0200

    correction or

--- public/APIManager.php
+++ public/APIManager.php
@@ -76,7 +76,7 @@ class APIManager
     private function deleteAction($url) {
         try {
             $response = $this->client->delete(sprintf(ADENTIFY_API_ROOT_URL, $url));
-            return $response->getStatusCode() == 200 | $response->getStatusCode() == 204;
+            return $response->getStatusCode() == 200 || $response->getStatusCode() == 204;
         } catch (\GuzzleHttp\Exception\ClientException $e) {
             return false;
         }
